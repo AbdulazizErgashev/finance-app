9f779d08626eb0c3d2f7c9972fe17898d84420c2
Students-g16 <gruppag783@gmail.com>
2025-03-05 13:38:06 +0500

    configurated the express

--- server/index.js
+++ server/index.js
@@ -7,4 +7,17 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 // configurations
-dotenv.config()
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// mongoose setup
+
+// https://www.youtube.com/watch?v=uoJ0Tv-BFcQ&t=1474s
+// 1:46:00
